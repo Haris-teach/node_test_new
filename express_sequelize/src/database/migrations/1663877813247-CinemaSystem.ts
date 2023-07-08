0ae534b496d215b42c1657880db9f84f90ffe077
Haris-teach <77834050+Haris-teach@users.noreply.github.com>
2023-07-08 13:12:08 +0500

    done

--- express_sequelize/src/database/migrations/1663877813247-CinemaSystem.ts
+++ express_sequelize/src/database/migrations/1663877813247-CinemaSystem.ts
@@ -1,5 +1,5 @@
-import { QueryInterface } from 'sequelize';
-
+import { literal,QueryInterface } from 'sequelize';
+import { ModelAttributes } from 'sequelize/types/model';
 export default {
   /**
    # ToDo: Create a migration that creates all tables for the following user stories
@@ -30,13 +30,153 @@ export default {
    * As a user I want to know where I'm sitting on my ticket
    * As a cinema owner I don't want to configure the seating for every show
    */
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  up: (queryInterface: QueryInterface): Promise<void> => {
-    throw new Error('TODO: implement migration in task 4');
-  },
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  down: (queryInterface: QueryInterface) => {
-    // do nothing
+
+up: async (queryInterface: QueryInterface) => {
+    await queryInterface.createTable('Movies', {
+      movie_id: {
+        type: 'integer',
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: { type: 'varchar' },
+      duration: { type: 'varchar' },
+     director:{type:'varchar'},
+    release_date:{type:'varchar'},
+      createdAt: {
+        type: 'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: 'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP'),
+      },
+    } as ModelAttributes);
+
+    await queryInterface.createTable('Shows',{
+      show_id: {
+        type: 'integer',
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      movie_id:{
+        
+    type: 'integer',
+    references: {
+      model:'Movies',
+      key: 'movie_id',
+    },
+      },
+      start_time:{
+        
+      type:'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP')
+      },
+     end_time:{
+      type: 'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP')
+      }, 
+      cinema_room_id:{
+        type:'integer',
+      }
+    }as ModelAttributes);
+
+
+await queryInterface.createTable('CinemaRooms',{
+  cinema_room_id:{
+    type:'intger',
+    primaryKey:true,
+    autoIncrement:true,
+  }, 
+  room_number:{
+    type:'varchar',
+  }, 
+  capacity:{
+type:'varchar'
+  }
+}as ModelAttributes);
+
+
+  await queryInterface.createTable('Pricing', {
+  pricing_id: {
+    type: 'integer',
+    primaryKey: true,
+    autoIncrement: true,
   },
+  show_id: {
+    type: 'integer',
+    references: {
+      model: 'Shows',
+      key: 'show_id',
+    },
+  },
+  price: {
+    type: 'varchar',
+  },
+} as ModelAttributes);
+
+ await queryInterface.createTable('SeatTypes',{
+  seat_type_id:{
+    type:'interger',
+   primaryKey: true,
+    autoIncrement: true,
+  },
+  type_name:{
+    type:'varchar',
+  }, 
+  premium_percentage:{
+    type:'varchar',
+  }
+ }as ModelAttributes);
+
+ await queryInterface.createTable('Seats',{
+
+seat_id:{
+   type:'interger',
+   primaryKey: true,
+    autoIncrement: true,
+},  cinema_room_id:{
+ 
+    type: 'integer',
+    references: {
+      model: 'CinemaRooms',
+      key: 'cinema_room_id',
+    },
+  
+}, seat_number:{
+  type:'varchar',
+}, seat_type_id:{
+  type:'integer'
+}
+
+ }as ModelAttributes);
+
+
+
+ await queryInterface.createTable('Bookings',{
+  booking_id:{
+     type:'interger',
+   primaryKey: true,
+    autoIncrement: true,
+  }, show_id:{
+    type: 'integer',
+    references: {
+      model: 'Shows',
+      key: 'show_id',
+    },
+  }, seat_id:{
+     type: 'integer',
+    references: {
+      model: 'Seats',
+      key: 'seat_id',
+    },
+  },  user_name:{
+    type:'varchar'
+  }, ticket_number:{
+    type:'varchar'
+  }
+ }as ModelAttributes)
+}
+
+
 };
